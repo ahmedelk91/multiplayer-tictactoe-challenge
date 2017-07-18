6ab3c537cc085fbef2c0de1502dcebb43492b82a
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2017-07-18 00:42:35 -0400

    Fixed static file rendering issue. Connection accepted via server.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello World!</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(3000, function(){
